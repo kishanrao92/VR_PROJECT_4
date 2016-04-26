8a9ed31a7cb519558b291f7bf0dc6db01ed8870d
EvilBhalu <goyalprateek.21@gmail.com>
2016-04-26 01:58:35 -0400

    doing shit
    working shit

---
+++ FinalPrjct/Assets/TalkRemy.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TalkRemy : MonoBehaviour {
+
+	Animator anim;
+	public iTweenEvent scvar;
+
+	// Use this for initialization
+	void Start(){
+		anim = GetComponent<Animator> ();
+		scvar = GetComponent<iTweenEvent> ();
+		StartCoroutine ("text1");
+	}
+
+	IEnumerator text1(){
+		yield return new WaitForSeconds (5.0f);
+		GameObject txtbx = GameObject.FindGameObjectWithTag ("text1");
+		GameObject cam = GameObject.FindGameObjectWithTag ("MainCamera");
+		txtbx.transform.position = cam.transform.position + new Vector3 (9.61f, -4.0f, -9.14f);
+	
+
+		anim.SetTrigger ("isTalking");
+
+		Destroy (txtbx, 7.0f);
+		StartCoroutine ("move1");
+	}
+
+	IEnumerator move1(){
+		yield return new WaitForSeconds (8.0f);
+
+		anim.SetTrigger ("isWalking");
+		scvar.enabled = true;
+
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
