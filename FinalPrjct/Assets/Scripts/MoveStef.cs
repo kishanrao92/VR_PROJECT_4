8dd9e5422d7b2011eb31055a14099294182d15f1
EvilBhalu <goyalprateek.21@gmail.com>
2016-04-26 20:27:36 -0400

    more shit
    getting lots of shit done

---
+++ FinalPrjct/Assets/Scripts/MoveStef.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveStef : MonoBehaviour {
+
+	Animator anim;
+	public iTweenEvent setscr;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		setscr = GetComponent<iTweenEvent> ();
+
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ FinalPrjct/Assets/Scripts/TalkRemy.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TalkRemy : MonoBehaviour {
+
+	public Animator anim;
+	public iTweenEvent scvar;
+
+	// Use this for initialization
+	void Start(){
+		anim = GetComponent<Animator> ();
+		scvar = GetComponent<iTweenEvent> ();
+		StartCoroutine ("text1");
+	}
+
+	IEnumerator text1(){
+		yield return new WaitForSeconds (5.0f);
+		GameObject txtbx = GameObject.FindGameObjectWithTag ("text1");
+		GameObject cam = GameObject.FindGameObjectWithTag ("MainCamera");
+		//txtbx.transform.position = cam.transform.position + new Vector3 (9.61f, -4.0f, -9.14f);
+		txtbx.transform.position = new Vector3(-13.78f,5.13f,-8.98f);
+		//txtbx.transform.rotation = new Quaternion (25.17577f, 180.0f, 1.349529e-12f);
+
+		anim.SetTrigger ("isTalking");
+
+		Destroy (txtbx, 7.0f);
+		StartCoroutine ("move1");
+	}
+
+	IEnumerator move1(){
+		yield return new WaitForSeconds (8.0f);
+
+		anim.SetTrigger ("isWalking");
+		scvar.enabled = true;
+
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
